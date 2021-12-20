e256a18b17ae50e7aef4ca213e554a9323cf6cf8
AnimMouse <git@animmouse.com>
2021-12-20 22:08:59 +0800

    Refactor scripts directory and YAML

--- scripts/sign-in/Windows.ps1
+++ scripts/sign-in/Windows.ps1
@@ -1,5 +1,5 @@
 Write-Host ::group::Signing in to Rclone using Rclone config
-New-Item "$env:APPDATA\rclone\" -ItemType Directory
+New-Item $env:APPDATA\rclone\ -ItemType Directory
 $rclone_config = [Convert]::FromBase64String($env:rclone_config)
 [IO.File]::WriteAllBytes("$env:APPDATA\rclone\rclone.conf", $rclone_config)
 Write-Host ::endgroup::
