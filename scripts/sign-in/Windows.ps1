a2a98c78c91c50bd3e6e547f8dfbc2d4e2ea3798
Steve Todorov <steve.todorov@carlspring.com>
2023-03-23 14:53:03 +0200

    Forcefully create rclone configuration directory.

--- scripts/sign-in/Windows.ps1
+++ scripts/sign-in/Windows.ps1
@@ -1,7 +1,9 @@
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 Write-Host ::group::Signing in to Rclone using Rclone config
-New-Item $env:APPDATA\rclone -ItemType Directory
+# This should use -Force, because gh runner might not be disposable instance (i.e. self-hosted)
+# Relates to https://github.com/AnimMouse/tool-cache/issues/3
+New-Item $env:APPDATA\rclone -ItemType Directory -Force
 $rclone_config = [Convert]::FromBase64String($env:rclone_config)
 [IO.File]::WriteAllBytes("$env:APPDATA\rclone\rclone.conf", $rclone_config)
 Write-Host ::endgroup::
