8fd08c3df1852c3be62b88de7d5888179d308721
AnimMouse <git@animmouse.com>
2023-03-24 16:01:23 +0800

    fix: Don't error when directory exists on Unix-like

--- scripts/sign-in/Windows.ps1
+++ scripts/sign-in/Windows.ps1
@@ -1,8 +1,6 @@
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 Write-Host ::group::Signing in to Rclone using Rclone config
-# This should use -Force, because gh runner might not be disposable instance (i.e. self-hosted)
-# Relates to https://github.com/AnimMouse/tool-cache/issues/3
 New-Item $env:APPDATA\rclone -ItemType Directory -Force
 $rclone_config = [Convert]::FromBase64String($env:rclone_config)
 [IO.File]::WriteAllBytes("$env:APPDATA\rclone\rclone.conf", $rclone_config)
