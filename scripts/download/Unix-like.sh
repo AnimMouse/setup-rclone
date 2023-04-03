b47afb7d299eb639618d2120059d8374065bd84a
AnimMouse <git@animmouse.com>
2023-04-03 20:17:24 +0800

    fix: Use native commands for download and extracting

--- scripts/download/Unix-like.sh
+++ scripts/download/Unix-like.sh
@@ -2,7 +2,7 @@
 set -eu
 echo ::group::Downloading Rclone $version for $RUNNER_OS
 if [ $RUNNER_OS = macOS ]; then os=osx; else os=linux; fi
-aria2c -x 16 -q $GITHUB_SERVER_URL/rclone/rclone/releases/download/$version/rclone-$version-$os-amd64.zip
-7z e rclone-$version-$os-amd64.zip rclone -oRclone -r
+wget -q $GITHUB_SERVER_URL/rclone/rclone/releases/download/$version/rclone-$version-$os-amd64.zip
+unzip -jq rclone-$version-$os-amd64.zip */rclone -d Rclone
 rm rclone-$version-$os-amd64.zip
 echo ::endgroup::
--- scripts/download/Windows.ps1
+++ scripts/download/Windows.ps1
@@ -1,7 +1,9 @@
 $ErrorActionPreference = 'Stop'
+$ProgressPreference = 'SilentlyContinue'
 Set-StrictMode -Version Latest
 Write-Host ::group::Downloading Rclone $env:version for Windows
-aria2c -x 16 -q $env:GITHUB_SERVER_URL/rclone/rclone/releases/download/$env:version/rclone-$env:version-windows-amd64.zip
-7z e rclone-$env:version-windows-amd64.zip rclone.exe -oRclone -r
-Remove-Item rclone-$env:version-windows-amd64.zip
+Invoke-WebRequest $env:GITHUB_SERVER_URL/rclone/rclone/releases/download/$env:version/rclone-$env:version-windows-amd64.zip -OutFile rclone-$env:version-windows-amd64.zip
+Expand-Archive rclone-$env:version-windows-amd64.zip Rclone
+Move-Item Rclone\rclone-$env:version-windows-amd64\* Rclone
+Remove-Item rclone-$env:version-windows-amd64.zip, Rclone\rclone-$env:version-windows-amd64
 Write-Host ::endgroup::
