aa64103486330a0c617762917b5d133c30a55ef5
Anim Mouse <git@animmouse.com>
2026-01-12 16:00:55 +0800

    feat: Add support for arm64 Windows

--- scripts/download/Windows.ps1
+++ scripts/download/Windows.ps1
@@ -1,9 +1,10 @@
 $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
 Set-StrictMode -Version Latest
-Write-Host ::group::Downloading Rclone $env:version for Windows
-Invoke-WebRequest $env:GITHUB_SERVER_URL/rclone/rclone/releases/download/$env:version/rclone-$env:version-windows-amd64.zip -OutFile rclone-$env:version-windows-amd64.zip
-Expand-Archive rclone-$env:version-windows-amd64.zip Rclone
-Move-Item Rclone\rclone-$env:version-windows-amd64\* Rclone
-Remove-Item rclone-$env:version-windows-amd64.zip, Rclone\rclone-$env:version-windows-amd64
+Write-Host ::group::Downloading Rclone $env:version for Windows $env:RUNNER_ARCH
+$arch = if ($env:RUNNER_ARCH -eq "ARM64") { "arm64" } else { "amd64" }
+Invoke-WebRequest $env:GITHUB_SERVER_URL/rclone/rclone/releases/download/$env:version/rclone-$env:version-windows-$arch.zip -OutFile rclone-$env:version-windows-$arch.zip
+Expand-Archive rclone-$env:version-windows-$arch.zip Rclone
+Move-Item Rclone\rclone-$env:version-windows-$arch\* Rclone
+Remove-Item rclone-$env:version-windows-$arch.zip, Rclone\rclone-$env:version-windows-$arch
 Write-Host ::endgroup::
